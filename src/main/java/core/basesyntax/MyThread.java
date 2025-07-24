1ef97bfe1d8a7cc369a88746cf745f411b4705e1
Anton <StarAntonU@Gmail.com>
2025-07-24 14:28:37 +0300

    done

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+
+        int duration = (int) (Math.random() * 1000);
+        return String.format("Task duration was %s ms, execution finished at %s",
+                duration,
+                LocalTime.now());
     }
 }
